e5f39f86ea2dd740169b53bcfe16c405e0fe1a2b
Yamini9984 <168056699+Yamini9984@users.noreply.github.com>
2024-08-10 17:48:32 +0530

    Add files via upload

---
+++ 42nr8sug3/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const resume = document.querySelector('.resume');
+
+    resume.addEventListener('click', function() {
+        alert('Thank you for viewing my resume!');
+    });
+});
